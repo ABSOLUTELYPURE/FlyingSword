e0df261d1410d28c176b9369896a6993010c5f28
RasdSky <skysunking@hotmail.com>
2018-02-05 22:01:25 +0800

    demo
    add scripts

---
+++ Assets/Scripts/Actor/Actor.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actor : MonoBehaviour
+{
+    private float m_hp;
+    private float m_def;
+    private float m_atk;
+    private string m_name;
+
+    public virtual string Name
+    {
+        get
+        {
+            return m_name;
+        }
+        set
+        {
+            m_name = value;
+        }
+    }
+    public virtual float Atk
+    {
+        get
+        {
+            return m_atk;
+        }
+
+        set
+        {
+            m_atk = value;
+        }
+    }
+    public virtual float HP
+    {
+        get
+        {
+            return m_hp;
+        }
+
+        set
+        {
+            m_hp = value;
+        }
+    }
+    public virtual float Def
+    {
+        get
+        {
+            return m_def;
+        }
+
+        set
+        {
+            m_def = value;
+        }
+    }
+}
---
+++ Assets/Scripts/Data/DataManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager:MonoBehaviour
+{
+    #region Instance
+    private static DataManager m_inst;
+    public static DataManager Instance
+    {
+        get
+        {
+            if (m_inst == null)
+            {
+                GameObject go = new GameObject("DataManager");
+                m_inst = go.AddComponent<DataManager>();
+                m_inst.Init();
+                DontDestroyOnLoad(go);
+            }
+            return m_inst;
+        }
+    }
+    #endregion
+
+    private bool m_isInited = false;
+
+    private void Init()
+    {
+        if(!m_isInited)
+        {
+            //todo
+        }
+    }
+}
